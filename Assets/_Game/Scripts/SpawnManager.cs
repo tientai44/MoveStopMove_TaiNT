f612c4f8eaad7beca36d4dbfd4cf89544f9a962e
tientai44 <97567774+tientai44@users.noreply.github.com>
2023-03-31 23:36:17 +0700

    update random equip for bot

--- Assets/_Game/Scripts/SpawnManager.cs
+++ Assets/_Game/Scripts/SpawnManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SpawnManager : GOSingleton<SpawnManager>
 {
-    private List<SetType> sets = new List<SetType> { SetType.Set1,SetType.Set2,SetType.Set3,SetType.Set4,SetType.Set5};
+    private List<SetType> sets = new List<SetType> { SetType.Set1, SetType.Set2, SetType.Set3, SetType.Set4, SetType.Set5 };
+    private List<HeadType> heads = new List<HeadType> { HeadType.Head1, HeadType.Head2, HeadType.Cowboy, HeadType.Crown, HeadType.Crown, HeadType.HatCap, HeadType.HeadPhone, HeadType.ArrowHead };
+    private List<ShieldType> shieldTypes = new List<ShieldType>{ShieldType.Shield1,ShieldType.Shield2};
+    
     public List<Transform> SpawnBot(int numBot)
     {
         List<Transform> transforms = new List<Transform>();
@@ -14,14 +17,31 @@ public class SpawnManager : GOSingleton<SpawnManager>
         GameController.GetInstance().CurrentPlayer = playerController;
         for (int i = 0; i < numBot; i++)
         {
+            
             if(i>= GameController.GetInstance().L_SpawnBot.Count)
             {
                 break;
             }
-            GameObject go= GameObjectPools.GetInstance().GetFromPool(CharacterType.Bot.ToString(), LevelManager.GetInstance().CurrentLevel.L_SpawnPos[i].position);
-            go.GetComponent<BotController>().ChangeEquipment(GameObjectPools.GetInstance().weapons[Random.Range(0, GameObjectPools.GetInstance().weapons.Count)]);
-            go.GetComponent<BotController>().SetFullSet(sets[Random.Range(0,sets.Count)]);
-            transforms.Add(go.transform);
+            BotController bot= GameObjectPools.GetInstance().GetFromPool(CharacterType.Bot.ToString(), LevelManager.GetInstance().CurrentLevel.L_SpawnPos[i].position).GetComponent<BotController>();
+
+            //random weapon
+            bot.ChangeEquipment(GameObjectPools.GetInstance().weapons[Random.Range(0, GameObjectPools.GetInstance().weapons.Count)]);
+            int select = Random.Range(0, 2);
+
+            if (select == 0)
+            {
+                //random skin
+                bot.SetFullSet(sets[Random.Range(0, sets.Count)]);
+            }
+            else
+            {
+                //random equip
+                bot.SetHead(heads[Random.Range(0,heads.Count)]);
+                bot.SetPant(GameObjectPools.GetInstance().pantMaterials[Random.Range(0, GameObjectPools.GetInstance().pantMaterials.Count)]);
+                bot.SetShield(shieldTypes[Random.Range(0,shieldTypes.Count)]);
+            }
+
+            transforms.Add(bot.TF);
         }
         return transforms;
     }
